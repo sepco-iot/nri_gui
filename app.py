b03260db6e2e70d6409920166e74be54e76008a9
mh-salari <mh.salari@sutech.ac.ir>
2019-06-29 22:06:32 +0430

    add bottuns to cahne time delta in grafana iframe

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-    #app.run(host='localhost', port=5000, debug=True)
-    app.run()
+    app.run(host='localhost', port=5000, debug=True)
+    #app.run()
 
 
 
